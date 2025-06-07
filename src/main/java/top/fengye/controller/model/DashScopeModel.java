8651332086ee26267f87eca6e3ace5712f28a1e3
fengye <fengye4302@gmail.com>
2025-06-07 18:39:05 +0800

    add call

--- src/main/java/top/fengye/controller/model/DashScopeModel.java
+++ src/main/java/top/fengye/controller/model/DashScopeModel.java
@@ -1,33 +1,102 @@
 package top.fengye.controller.model;
 
+import com.alibaba.dashscope.aigc.generation.GenerationOutput;
 import com.alibaba.dashscope.aigc.generation.GenerationParam;
+import com.alibaba.dashscope.aigc.generation.GenerationResult;
+import com.alibaba.dashscope.aigc.generation.GenerationUsage;
 import com.alibaba.dashscope.common.Message;
 import com.alibaba.dashscope.common.Role;
+import com.alibaba.dashscope.exception.ApiException;
+import com.alibaba.dashscope.exception.InputRequiredException;
+import com.alibaba.dashscope.exception.NoApiKeyException;
+import com.alibaba.dashscope.tools.*;
+import com.alibaba.dashscope.utils.JsonUtils;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.github.victools.jsonschema.generator.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.messages.AssistantMessage;
 import org.springframework.ai.chat.messages.ToolResponseMessage;
+import org.springframework.ai.chat.metadata.ChatGenerationMetadata;
+import org.springframework.ai.chat.metadata.ChatResponseMetadata;
+import org.springframework.ai.chat.metadata.DefaultUsage;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.model.Generation;
 import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.model.tool.ToolCallingChatOptions;
+import org.springframework.ai.tool.ToolCallback;
+import org.springframework.ai.tool.definition.ToolDefinition;
+import org.springframework.util.CollectionUtils;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author: FengYe
  * @date: 2025/6/5 02:40
  * @description: BailianModel
  */
+@Slf4j
 public class DashScopeModel implements ChatModel {
     @Override
     public ChatResponse call(Prompt prompt) {
-        return null;
+        GenerationParam generationParam = convertDashScopeParam(prompt);
+        com.alibaba.dashscope.aigc.generation.Generation gen = new com.alibaba.dashscope.aigc.generation.Generation();
+        GenerationResult res = null;
+        try {
+            res = gen.call(generationParam);
+        } catch (Exception e) {
+            log.error("DashScopeModel call error", e);
+            return null;
+        }
+        return convertDashScopeResponse(res);
+    }
+
+    private ChatResponse convertDashScopeResponse(GenerationResult res) {
+        GenerationOutput.Choice choice = res.getOutput().getChoices().getFirst();
+        if (null == choice) {
+            throw new IllegalArgumentException("output is null");
+        }
+        // 1.构造AssistantMessage
+        List<ToolCallBase> dsTool = choice.getMessage().getToolCalls();
+        AssistantMessage assistantMessage = null;
+        if (CollectionUtils.isEmpty(dsTool)) {
+            assistantMessage = new AssistantMessage(choice.getMessage().getContent());
+        } else {
+            List<AssistantMessage.ToolCall> list = dsTool.stream().map(t -> {
+                ToolCallFunction toolCallFunction = (ToolCallFunction) t;
+                return new AssistantMessage.ToolCall(toolCallFunction.getId(), toolCallFunction.getFunction().getName(),
+                        toolCallFunction.getFunction().getName(), toolCallFunction.getFunction().getArguments());
+            }).toList();
+
+            // 按需添加
+            HashMap<String, Object> properties = new HashMap<>();
+            assistantMessage = new AssistantMessage(choice.getMessage().getContent(), properties, list);
+        }
+
+        // 2.构造Generation
+        ChatGenerationMetadata chatGenerationMetadata = ChatGenerationMetadata.builder().finishReason(choice.getFinishReason()).build();
+        Generation generation = new Generation(assistantMessage, chatGenerationMetadata);
+
+        // 3.构造ChatResponse
+        GenerationUsage dsUsage = res.getUsage();
+        DefaultUsage usage = new DefaultUsage(dsUsage.getInputTokens(), dsUsage.getOutputTokens(), dsUsage.getTotalTokens());
+        ChatResponseMetadata chatResponseMetadata = ChatResponseMetadata.builder()
+                .usage(usage)
+                .id(res.getRequestId()).build();
+        return new ChatResponse(List.of(generation), chatResponseMetadata);
     }
 
     private GenerationParam convertDashScopeParam(Prompt prompt) {
         List<org.springframework.ai.chat.messages.Message> messages = prompt.getInstructions();
         ChatOptions options = prompt.getOptions();
+        if (null == options) {
+            throw new IllegalArgumentException("options is null");
+        }
 
         // 1. 处理大模型 options
         GenerationParam.GenerationParamBuilder<?, ?> paramBuilder = GenerationParam.builder()
@@ -73,8 +142,26 @@ public class DashScopeModel implements ChatModel {
             }
         }).flatMap(List::stream).collect(Collectors.toList()));
 
+        // 3.处理大模型 functionCall
+        if (options instanceof ToolCallingChatOptions toolCallingChatOptions) {
+            List<ToolBase> dashscopeFunctions = new ArrayList<>();
+            List<ToolCallback> toolCallbacks = toolCallingChatOptions.getToolCallbacks();
+            toolCallbacks.forEach(toolCallback -> {
+                ToolDefinition toolDefinition = toolCallback.getToolDefinition();
+                dashscopeFunctions.add(ToolFunction.builder().function(FunctionDefinition.builder()
+                        .name(toolDefinition.name())
+                        .description(toolDefinition.description())
+                        .parameters(JsonUtils.parseString(toolDefinition.inputSchema()).getAsJsonObject())
+                        .build()
+                ).build());
+            });
+            paramBuilder.tools(dashscopeFunctions);
+        }
+
+
         return paramBuilder.build();
     }
+
 
     public static void main(String[] args) {
 //        try {
